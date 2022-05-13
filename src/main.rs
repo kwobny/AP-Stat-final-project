4baee7ab42d7e8165131689548b507d20f434080
Yeongjin Kwon <yeongjinkwon@gmail.com>
2022-05-12 22:38:57 -0400

    Add random functionality to project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use rand::distributions::{Distribution, Uniform};
+
 fn main() {
-    println!("Hello, world!");
+    get_pi(3);
+}
+
+fn get_pi(num_points: u32) {
+    let mut rng = rand::thread_rng();
+    let distribution: Uniform<f64> = Uniform::new(0.0, 1.0);
+
+    let random_number = distribution.sample(&mut rng);
+    println!("{}", random_number);
 }
