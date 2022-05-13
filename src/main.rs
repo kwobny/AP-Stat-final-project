e90f4deaf5c5cb0155b58bfed24935e014da7b3e
Yeongjin Kwon <yeongjinkwon@gmail.com>
2022-05-13 16:00:29 -0400

    Tweak some things
    Make program print proportion instead of calculated value of pi.
    Make program use <= instead of < when testing if in circle.

--- src/main.rs
+++ src/main.rs
@@ -29,14 +29,14 @@ fn main() {
             }
         };
 
-        let pi_s: Vec<_> = (0..trials).map(|_| get_pi(points)).collect();
+        let pi_s: Vec<_> = (0..trials).map(|_| get_pi_proportion(points)).collect();
         println!("{:?}", pi_s);
 
         break;
     }
 }
 
-fn get_pi(num_points: u32) -> f64 {
+fn get_pi_proportion(num_points: u32) -> f64 {
     let mut rng = rand::thread_rng();
     let distribution: Uniform<f64> = Uniform::new(0.0, 1.0);
     let mut points_in_circle: u32 = 0;
@@ -46,14 +46,13 @@ fn get_pi(num_points: u32) -> f64 {
             distribution.sample(&mut rng),
             distribution.sample(&mut rng),
         );
-        let is_in_circle = (point.0.powi(2) + point.1.powi(2)).sqrt() < 1.0;
+        let is_in_circle = (point.0.powi(2) + point.1.powi(2)).sqrt() <= 1.0;
         if is_in_circle {
             points_in_circle += 1;
         }
     }
 
     let proportion = (points_in_circle as f64)/(num_points as f64);
-    let pi = proportion * 4.0;
-
-    return pi;
+    
+    return proportion;
 }
